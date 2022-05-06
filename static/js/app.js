22335185b9fe94f2dfb1a278b7208f3757047db3
Armstrong <Scott.Armstrong@viasat.com>
2022-05-06 16:17:56 -0700

    created table 11.5.2 complete

--- static/js/app.js
+++ static/js/app.js
@@ -4,3 +4,24 @@ const tableData = data;
 //Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
+function buildTable(data) {
+    //Clear any existing data from the table that already exists
+    tbody.html("");
+
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+        //tells JavaScript to find the <tbody> tag within the HTML and add a table row ("tr")
+        // i.e. Append a row to the table body
+        let row = tbody.append("tr");
+        
+        // Loop through each field in the dataRow and add
+        // each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            //append each value of the object to a cell in the table
+            let cell = row.append("td");
+            cell.text(val);
+            }
+        );
+    });
+}
